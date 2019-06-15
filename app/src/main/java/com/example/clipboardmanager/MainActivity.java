e7e29c8f7adb49172a40c8369527fd88dcd4e8d9
paranoid <abyk888@gmail.com>
2019-06-15 20:21:59 +0530

    *

--- app/src/main/java/com/example/clipboardmanager/MainActivity.java
+++ app/src/main/java/com/example/clipboardmanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.clipboardmanager;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Intent i = new Intent(this, ClipBoardService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(i);
+        } else {
+            startService(i);
+        }
     }
 }
